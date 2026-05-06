8800968dbf55ee25eb55d22cbc8d9dfb9e89bb13
User 1 <jdcasteel2000@gmail.com>
2026-05-05 23:21:55 -0400

    Day 4: Complete overhaul to script loading + Hitbox component

--- test.js
+++ test.js
@@ -1,1 +1,14 @@
 //Test code blah blah
+/*
+==========================TODO==========================
+Priority:
+ - Further develop hitboxes
+    - Add more event types (onEnter, onExit, etc.)
+    - Add tags to hitboxes for more specific collision handling
+
+ - Scene management (scene transitions, loading/unloading assets)
+
+ - WebGL rendering (shaders, textures, sprites, etc.)
+
+ - Audio management (playing sounds, music, etc.)
+*/
